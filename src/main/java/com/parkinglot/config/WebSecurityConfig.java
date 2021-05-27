f694dff3edc8deb852dee6c4bbe9aeab565d5e68
Jung <kqt20@naver.com>
2021-05-27 20:45:40 +0900

    [fix] 아두이노 이중주차 권한 수정

--- src/main/java/com/parkinglot/config/WebSecurityConfig.java
+++ src/main/java/com/parkinglot/config/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/signin","/signup","/parking").permitAll().
+                .authorizeRequests().antMatchers("/signin","/signup","/parking","/parking-double").permitAll().
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
